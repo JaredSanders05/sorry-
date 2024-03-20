ec6ab6828b942c7d71fa8b7a2c726f2dbfe5f3b1
Ahad-Chughtai <ahadchughtai007@gmail.com>
2024-03-20 13:44:39 -0500

    Added SCripts

---
+++ Sorry/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public MonoScript Box;
+    List<MonoScript> boxes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxes = new List<MonoScript>();
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Sorry/Assets/Scripts/Piece.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public string Color;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    Piece() 
+    { 
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
