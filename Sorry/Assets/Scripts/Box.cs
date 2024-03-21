fde3e4cd0e74f2fbd5d602b7d0923092a18a0f54
Ahad-Chughtai <ahadchughtai007@gmail.com>
2024-03-21 13:45:11 -0500

    Update Box.cs

--- Sorry/Assets/Scripts/Box.cs
+++ Sorry/Assets/Scripts/Box.cs
@@ -6,24 +6,17 @@ public class Box : MonoBehaviour
 {
     public GameObject piece;
     public string type;
-    public int y;
-    public int x;
-    public int z;
+  
 
     Box(int x, int y, int z)
     {
-        this.y = y;     
-        this.x = x;
-        this.z = z;
         piece = null;
         type = "white";
     }
 
     Box(int x, int y, int z, string type) 
     { 
-        this.x=x;
-        this.y=y;
-        this.z=z;
+   
         piece = null;
         this.type = type;
     }
