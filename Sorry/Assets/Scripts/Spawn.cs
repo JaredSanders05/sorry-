7222f2ccccf609fbf0359d1fa28a7641892a5847
Ahad-Chughtai <ahadchughtai007@gmail.com>
2024-03-20 21:29:50 -0500

    Update Spawn.cs

--- Sorry/Assets/Scripts/Spawn.cs
+++ Sorry/Assets/Scripts/Spawn.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    List<GameObject> Pieces;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
