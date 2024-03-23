a84e2ed935cfbb9f95ed5673f450e424df94c759
Skullblitz <Jaredsanders1127@gmail.com>
2024-03-23 16:27:02 -0500

    Made script get a list of objects with Camera tag and can move to each camera based on a public function that should be able to be called outside of the script

--- Sorry/Assets/Scripts/CameraMovement.cs
+++ Sorry/Assets/Scripts/CameraMovement.cs
@@ -4,31 +4,50 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform pos1;
+    private List<Transform> positions = new List<Transform>();
+    public int cam;
     public float duration = 5f; // Duration in seconds over which the object will move
     public bool conditionToCheck = true; // Runs this script if move is true
-    
-    void Update(){
-        if(conditionToCheck){
-           StartCoroutine(Move());
+    void Start()
+    {
+        foreach (GameObject go in GameObject.FindGameObjectsWithTag("Camera"))
+        {
+            positions.Add(go.GetComponent<Transform>());
         }
     }
-    IEnumerator Move(){
+    void Update()
+    {
+        if(conditionToCheck)
+        {
+            StartCoroutine(Move());
+        }
+    }
+    IEnumerator Move()
+    {
         Vector3 startPosition = transform.position; // Store the starting position
         Quaternion startRotation = transform.rotation; // Store the starting rotation
         float time = 0;
 
         while (time < duration)
         {
-            transform.position = Vector3.Lerp(startPosition, pos1.position, time / duration);
-            transform.rotation = Quaternion.Lerp(startRotation, pos1.rotation, time / duration);
+            transform.position = Vector3.Lerp(startPosition, positions[cam].position, time / duration);
+            transform.rotation = Quaternion.Lerp(startRotation, positions[cam].rotation, time / duration);
             time += Time.deltaTime;
             yield return null;
         }
 
         // Ensure the position is set to the target position after the duration
-        transform.position = pos1.position;
-        transform.rotation = pos1.rotation;
+        transform.position = positions[cam].position;
+        transform.rotation = positions[cam].rotation;
+        if(cam == 1)
+            changeCam(cam+1);
     }
-    
+    public void changeCam(int index)
+    {
+        if(index >= positions.Count)
+            cam = 0;
+        else
+            cam = index;
+        StartCoroutine(Move());
+    }
 }
